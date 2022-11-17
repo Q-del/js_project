e294aec8bfd3711324a81d9838badba9467eb937
Roman <roman.tsvetkov@glidewelldental.com>
2022-11-17 19:56:23 +0400

    first commit

--- learn.js
+++ learn.js
@@ -1,8 +1,15 @@
-alert('1')
-const res = confirm("Are u gay?")
-if(res) {
-    alert('Nahui')
+//1
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?")
+//2
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
 }
-else {
-    alert('Good')
-}
+//3
+let lastFilm = prompt('Один из последних просмотренных фильмов?')
+let score = prompt('На сколько вы его оцениваете?')
+personalMovieDB.movies[lastFilm] = +score
+console.log(personalMovieDB)
